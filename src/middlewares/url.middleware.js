b9186b32c1f76aec7f82e65d4d4bdd7634522918
Galliss13 <matheusgalindo1313@gmail.com>
2022-12-20 15:05:40 -0300

    feat: urlSchemaValidation

--- src/middlewares/url.middleware.js
+++ src/middlewares/url.middleware.js
@@ -1,7 +1,13 @@
 import connection from "../database/database.js"
+import {urlSchema} from "../schemas/urlSchemas.js"
 
 export async function urlSchemaValidation(req, res, next) {
-
+    const url = req.body
+    const {error} = urlSchema.validate(url, {abortEarly: false})
+    if (error) {
+        const errors = error.details.map((detail) => detail.message)
+        return res.status(422).send(errors)
+    }
     next()
 }
 
