34abc36247901e85c810183bdc1ca0353a45d41b
Galliss13 <matheusgalindo1313@gmail.com>
2022-12-20 16:01:36 -0300

    feat: postUrlShorten

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -1,8 +1,14 @@
 import connection from "../database/database.js";
+import { nanoid } from "nanoid";
 
 export async function postUrlShorten(req, res) {
+    const {user} = res.locals
+    const {url} = req.body 
+    const shortUrl = nanoid(5)
     try {
-        
+        await connection.query('INSERT INTO urls ("userId", url, "shortUrl", "visitCount") VALUES ($1, $2, $3, 0)', 
+        [user.id, url, shortUrl])
+        return res.sendStatus(201)
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
