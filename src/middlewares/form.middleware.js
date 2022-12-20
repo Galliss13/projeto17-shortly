da855e8eadb82bc6fdc68968610e6c2faf8d1a61
Galliss13 <matheusgalindo1313@gmail.com>
2022-12-20 12:49:19 -0300

    feat: signupSchemaValidation

--- src/middlewares/form.middleware.js
+++ src/middlewares/form.middleware.js
@@ -1,7 +1,13 @@
 import connection from "../database/database"
+import { signupSchema } from "../schemas/formSchemas"
 
 export function signupSchemaValidation(req, res, next) {
-
+    const user = req.body
+    const {error} = signupSchema.validate(user, {abortEarly: false})
+    if (error) {
+        const errors = error.details.map((detail) => detail.message)
+        return res.status(422).send(errors)
+    }
     next()
 }
 
